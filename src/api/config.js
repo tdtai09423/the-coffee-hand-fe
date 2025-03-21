0a51a78794e2ccc34c10ed5e11609aff37fadd11
tdtai09423 <tranduytai7@gmail.com>
2025-03-21 20:07:42 +0700

    fix BE URL v2

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:7099/api'
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
 };
 
 export default config;

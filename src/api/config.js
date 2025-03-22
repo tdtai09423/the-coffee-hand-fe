aad37d2e1024fdced3e9459978ce0dcaa2d4bb3b
tdtai09423 <tranduytai7@gmail.com>
2025-03-22 11:09:51 +0700

    BE URL v3

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://thecoffeehand20250321114154.azurewebsites.net/api',
 };
 
 export default config;
